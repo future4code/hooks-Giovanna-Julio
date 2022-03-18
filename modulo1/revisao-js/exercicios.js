8f9cca5e7747bf3771f286632355537333e28598
Giovanna Galhardo Julio <gigalhardo98@gmail.com>
2022-03-18 18:37:08 -0300

    ex7

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -4,82 +4,113 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-  return array.length;
+    return array.length;
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  return array.reverse();
+    return array.reverse();
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  return array.sort((a, b) => a - b);
+    return array.sort((a, b) => a - b);
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  let novoArray = array.filter((array) => {
-    return array % 2 === 0;
-  });
+    let novoArray = array.filter((array) => {
+        return array % 2 === 0;
+    });
 
-  return novoArray;
+    return novoArray;
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
-  let novoArray = array.filter((array) => {
-    return array % 2 === 0;
-  });
+    let novoArray = array.filter((array) => {
+        return array % 2 === 0;
+    });
 
-  let arrayElevado = novoArray.map((novoArray) => {
-    return Math.pow(novoArray, 2);
-  });
+    let arrayElevado = novoArray.map((novoArray) => {
+        return Math.pow(novoArray, 2);
+    });
 
-  return arrayElevado;
+    return arrayElevado;
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  let i = 0;
-  let maior = -Infinity;
-  for (i; i < array.length; i++) {
-    if (array[i] > maior) {
-      maior = array[i];
+    let i = 0;
+    let maior = -Infinity;
+    for (i; i < array.length; i++) {
+        if (array[i] > maior) {
+            maior = array[i];
+        }
     }
-  }
-  return maior;
+    return maior;
 }
 
 // EXERCÍCIO 07
-function retornaObjetoEntreDoisNumeros(num1, num2) {}
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+    let maior
+    let menor
+
+    if (num1 > num2) {
+        maior = num1
+        menor = num2
+    } else if (num2 > num1) {
+        maior = num2
+        menor = num1
+    } else {
+        maior = num1
+        menor = num2
+    }
+
+    let divisivel
+
+    if (maior % menor === 0) {
+        divisivel = true
+    } else {
+        divisivel = false
+    }
+
+    let objetoNumerico = {
+        maiorNumero: maior,
+        maiorDivisivelPorMenor: divisivel,
+        diferenca: maior - menor
+    }
+
+    return objetoNumerico
+}
 
 // EXERCÍCIO 08
-function retornaNPrimeirosPares(n) {}
+function retornaNPrimeirosPares(n) { }
 
 // EXERCÍCIO 09
-function classificaTriangulo(ladoA, ladoB, ladoC) {}
+function classificaTriangulo(ladoA, ladoB, ladoC) { }
 
 // EXERCÍCIO 10
-function retornaSegundoMaiorESegundoMenor(array) {}
+function retornaSegundoMaiorESegundoMenor(array) { }
 
 // EXERCÍCIO 11
-function retornaChamadaDeFilme(filme) {}
+function retornaChamadaDeFilme(filme) { }
 
 // EXERCÍCIO 12
-function retornaPessoaAnonimizada(pessoa) {}
+function retornaPessoaAnonimizada(pessoa) { }
 
 // EXERCÍCIO 13A
-function retornaPessoasAutorizadas(pessoas) {}
+function retornaPessoasAutorizadas(pessoas) { }
 
 // EXERCÍCIO 13B
-function retornaPessoasNaoAutorizadas(pessoas) {}
+function retornaPessoasNaoAutorizadas(pessoas) { }
 
 // EXERCÍCIO 14
-function retornaContasComSaldoAtualizado(contas) {}
+function retornaContasComSaldoAtualizado(contas) { }
 
 // EXERCÍCIO 15A
-function retornaArrayOrdenadoAlfabeticamente(consultas) {}
+function retornaArrayOrdenadoAlfabeticamente(consultas) { }
 
 // EXERCÍCIO 15B
-function retornaArrayOrdenadoPorData(consultas) {}
+function retornaArrayOrdenadoPorData(consultas) { }
