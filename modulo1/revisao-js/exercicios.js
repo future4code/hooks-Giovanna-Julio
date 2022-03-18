542eab96c533d4837b0b0eb8b34f4ad585c2baf2
Giovanna Galhardo Julio <gigalhardo98@gmail.com>
2022-03-18 18:57:39 -0300

    ex8

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -86,8 +86,19 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 }
 
 // EXERCÍCIO 08
-function retornaNPrimeirosPares(n) { }
+function retornaNPrimeirosPares(n) {
+    let arr = [];
 
+    for (let i = 0; i < n; i++) {
+        if (i % 2 === 0) {
+            arr.push(i);
+        } else {
+            n++
+        }
+    }
+
+    return arr
+}
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) { }
 
