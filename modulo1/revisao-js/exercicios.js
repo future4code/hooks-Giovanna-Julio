e9a708508731dfcd36baff26609ce747b60bb782
Giovanna Galhardo Julio <gigalhardo98@gmail.com>
2022-03-17 21:40:31 -0300

    prettier

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -1,109 +1,85 @@
 // ATENÇÃO!!!
-//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!!
 //    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
-
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-    return array.length
+  return array.length;
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-    return array.reverse()
+  return array.reverse();
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-    return array.sort((a, b) => a - b)
+  return array.sort((a, b) => a - b);
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-    let novoArray = array.filter((array) => {
-        return array % 2 === 0
-    })
+  let novoArray = array.filter((array) => {
+    return array % 2 === 0;
+  });
 
-    return novoArray
-
+  return novoArray;
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
-    let novoArray = array.filter((array) => {
-        return (array % 2) === 0
-    })
+  let novoArray = array.filter((array) => {
+    return array % 2 === 0;
+  });
 
-    let arrayElevado = novoArray.map((novoArray) => {
-        return Math.pow(novoArray, 2)
-    })
+  let arrayElevado = novoArray.map((novoArray) => {
+    return Math.pow(novoArray, 2);
+  });
 
-    return arrayElevado    
+  return arrayElevado;
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-    let i = 0
-    let maior = -Infinity
-    for (i; i < array.length; i++) {
-        if (array[i] > maior) {
-            maior = array[i]
-        }
+  let i = 0;
+  let maior = -Infinity;
+  for (i; i < array.length; i++) {
+    if (array[i] > maior) {
+      maior = array[i];
     }
-    return maior
+  }
+  return maior;
 }
 
 // EXERCÍCIO 07
-function retornaObjetoEntreDoisNumeros(num1, num2) {
-
-}
+function retornaObjetoEntreDoisNumeros(num1, num2) {}
 
 // EXERCÍCIO 08
-function retornaNPrimeirosPares(n) {
-
-}
+function retornaNPrimeirosPares(n) {}
 
 // EXERCÍCIO 09
-function classificaTriangulo(ladoA, ladoB, ladoC) {
-
-}
+function classificaTriangulo(ladoA, ladoB, ladoC) {}
 
 // EXERCÍCIO 10
-function retornaSegundoMaiorESegundoMenor(array) {
-
-}
+function retornaSegundoMaiorESegundoMenor(array) {}
 
 // EXERCÍCIO 11
-function retornaChamadaDeFilme(filme) {
-
-}
+function retornaChamadaDeFilme(filme) {}
 
 // EXERCÍCIO 12
-function retornaPessoaAnonimizada(pessoa) {
-
-}
+function retornaPessoaAnonimizada(pessoa) {}
 
 // EXERCÍCIO 13A
-function retornaPessoasAutorizadas(pessoas) {
-
-}
+function retornaPessoasAutorizadas(pessoas) {}
 
 // EXERCÍCIO 13B
-function retornaPessoasNaoAutorizadas(pessoas) {
-
-}
+function retornaPessoasNaoAutorizadas(pessoas) {}
 
 // EXERCÍCIO 14
-function retornaContasComSaldoAtualizado(contas) {
-
-}
+function retornaContasComSaldoAtualizado(contas) {}
 
 // EXERCÍCIO 15A
-function retornaArrayOrdenadoAlfabeticamente(consultas) {
-
-}
+function retornaArrayOrdenadoAlfabeticamente(consultas) {}
 
 // EXERCÍCIO 15B
-function retornaArrayOrdenadoPorData(consultas) {
-
-}
+function retornaArrayOrdenadoPorData(consultas) {}
