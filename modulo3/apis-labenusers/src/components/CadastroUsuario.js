80f8bac870d5b560dbd6c6439fd58606c33e8eba
Giovanna Galhardo Julio <gigalhardo98@gmail.com>
2022-05-24 18:07:03 -0300

    finalizando a parte da lista e deixando bonitinho

--- modulo3/apis-labenusers/src/components/CadastroUsuario.js
+++ modulo3/apis-labenusers/src/components/CadastroUsuario.js
@@ -2,58 +2,86 @@ import React from "react";
 import styled from "styled-components";
 import axios from "axios";
 
-const urlUsuarios = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
-
-const headers = {
-    headers: {
-      Authorization: "giovanna-julio-hooks"
-    }
-  };
-
+const Cadastro = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 100px;
+  border: 1px solid grey;
+  justify-content: center;
+  align-items: center;
+  color: orange;
+`;
 
 class CadastroUsuario extends React.Component {
-    
-    state = {
-        emailInput: "",
-        nomeInput: ""
-    }
+  state = {
+    emailInput: "",
+    nomeInput: "",
+  };
 
-    onChangeEmail = (event) => {
-        this.setState({emailInput: event.target.value})
-    }
+  onChangeEmail = (event) => {
+    this.setState({ emailInput: event.target.value });
+    console.log(this.state.emailInput);
+  };
 
-    onChangeNome = (event) => {
-        this.setState({nomeInput: event.target.value})
-    }
+  onChangeNome = (event) => {
+    this.setState({ nomeInput: event.target.value });
+    console.log(this.state.nomeInput);
+  };
 
-    criarUsuario = () => {
-        const body = {
-            name: this.state.nomeInput,
-            email: this.state.emailInput
-        }
+  criarUsuario = () => {
+    const urlUsuarios =
+      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
 
-        axios.post(urlUsuarios, body, headers)
-            .then((response) => {
-                alert("Usuário criado com sucesso!")
-                this.setState({emailInput: ""})
-                this.setState({nomeInput: ""})
-            })
-            .catch((err) => {
-                alert("Erro inesperado, tente novamente mais tarde.")
-                this.setState({emailInput: ""})
-                this.setState({nomeInput: ""})
-            })
-    }
-    
-    render(){
-        return(
-            <>
-                <input placeholder="E-mail" value={this.state.emailInput} onChange={this.onChangeEmail}/>
-                <input placeholder="Nome" value={this.state.nomeInput} onChange={this.onChangeNome}/>
-                <button onClick={this.criarUsuario}>Enviar</button>
-            </>
-        )
-    }
+    const headers = {
+      headers: {
+        Authorization: "giovanna-julio-hooks",
+      },
+    };
+
+    const body = {
+      name: this.state.nomeInput,
+      email: this.state.emailInput,
+    };
+
+    axios
+      .post(urlUsuarios, body, headers)
+      .then((response) => {
+        alert("Usuário criado com sucesso!");
+        console.log(response.data);
+        this.setState({ emailInput: "" });
+        this.setState({ nomeInput: "" });
+      })
+      .catch((error) => {
+        alert("Erro inesperado, tente novamente mais tarde.");
+        console.log(error.response);
+        this.setState({ emailInput: "" });
+        this.setState({ nomeInput: "" });
+      });
+  };
+
+  render() {
+    return (
+      <Cadastro>
+        <h2>Labenusers Cadastro</h2>
+        
+        <input
+          placeholder="E-mail"
+          value={this.state.emailInput}
+          onChange={this.onChangeEmail}
+        />
+        
+        <input
+          placeholder="Nome"
+          value={this.state.nomeInput}
+          onChange={this.onChangeNome}
+        />
+        
+        <br/>
+        
+        <button onClick={this.criarUsuario}>Enviar</button>
+      </Cadastro>
+    );
+  }
 }
 
 export default CadastroUsuario;
--- modulo3/apis-labenusers/src/components/ListaUsuarios.js
+++ modulo3/apis-labenusers/src/components/ListaUsuarios.js
@@ -2,15 +2,83 @@ import React from "react";
 import styled from "styled-components";
 import axios from "axios";
 
+const DisplayUsuarios = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 100px;
+  border: 1px solid grey;
+  justify-content: center;
+  align-items: center;
+  color: orange;
+  padding-top: 10px;
+`;
 
 class ListaUsuarios extends React.Component {
-    render(){
-        return(
-            <>
-            
-            </>
-        )
-    }
+  state = {
+    listaUsuarios: [],
+  };
+
+  showLista = () => {
+    const urlUsuarios =
+      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
+
+    const headers = {
+      headers: {
+        Authorization: "giovanna-julio-hooks",
+      },
+    };
+
+    axios
+      .get(urlUsuarios, headers)
+      .then((response) => {
+        this.setState({ listaUsuarios: response.data });
+      })
+      .catch((error) => {
+        alert("Erro inesperado!");
+      });
+  };
+
+  componentDidMount() {
+    this.showLista();
+  }
+
+  deletaUsuario = (id) => {
+    const urlUsuarioIndividual = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`;
+
+    const headers = {
+      headers: {
+        Authorization: "giovanna-julio-hooks",
+      },
+    };
+
+    axios
+      .delete(urlUsuarioIndividual, headers)
+      .then((response) => {
+        alert("Usuário deletado com sucesso!");
+      })
+      .catch((error) => {
+        alert("Tente novamente :(");
+      });
+  };
+
+  render() {
+    const usuarios = this.state.listaUsuarios.map((usuario) => {
+      return (
+        <div key={usuario.id}>
+          <p>{usuario.name}</p>
+          
+          <button onClick={() => this.deletaUsuario(usuario.id)}>Deletar</button>
+        </div>
+      );
+    });
+
+    return (
+      <DisplayUsuarios>
+        <h2>Labenusers já cadastrados:</h2>
+        {usuarios}
+      </DisplayUsuarios>
+    );
+  }
 }
 
 export default ListaUsuarios;
