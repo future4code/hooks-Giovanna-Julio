142090d5b855e2a6839fa6bf5dab18b6a67de5cc
Giovanna Galhardo Julio <gigalhardo98@gmail.com>
2022-05-30 16:12:38 -0300

    whatslab finalmente

--- modulo3/whatslab-projeto/src/App.js
+++ modulo3/whatslab-projeto/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import SecaoInput from "./components/SecaoInput";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image: url(https://browsecat.net/sites/default/files/desert-4k-sand-wallpapers-41669-468145-2834994.png);
+  background-size: 1400px 620px;
+  background-repeat: no-repeat;
+`;
+
+const SecaoMensagens = styled.div`
+  border: 1px rgb(30, 129, 176) solid;
+  width: 500px;
+  height: 594px;
+  background-color: rgb(30, 129, 176, 0.2);
+  border-radius: 15px;
+  color: #2a50bf;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Nome = styled.p`
+  font-weight: bold;
+`;
+const MensagemCompleta = styled.p`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 16px;
+  margin-right: 16px;
+  background-color: #f9bd74;
+  border-radius: 20px;
+`;
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      mensagens: [],
+    };
+  }
+
+  adicionarMensagens = (message) => {
+    this.setState({ mensagens: [...this.state.mensagens, message] });
+  };
+
+  render() {
+    const listaRenderizada = this.state.mensagens.map((mensagem, index) => {
+      return (
+        <MensagemCompleta key={index}>
+          <Nome>{mensagem.usuario}:&emsp;</Nome>
+          {mensagem.mensagem}
+        </MensagemCompleta>
+      );
+    });
+
+    return (
+      <Container>
+        <SecaoMensagens>{listaRenderizada}</SecaoMensagens>
+        
+        <SecaoInput adicionarMensagens={this.adicionarMensagens} />
+      </Container>
+    );
+  }
 }
 
 export default App;
