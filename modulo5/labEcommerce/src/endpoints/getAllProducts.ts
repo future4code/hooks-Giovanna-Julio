126102e2f8851187141c88d51c4b609c08585bca
Giovanna Galhardo Julio <gigalhardo98@gmail.com>
2022-09-20 17:53:07 -0300

    desafio e testes

--- modulo5/labEcommerce/src/endpoints/getAllProducts.ts
+++ modulo5/labEcommerce/src/endpoints/getAllProducts.ts
@@ -5,13 +5,71 @@ export const getAllProducts = async (req: Request, res: Response): Promise<void>
 	let errorCode = 400;
 
 	try {
-        const allProducts = await connection("labecommerce_products").select("*");
-        if (!allProducts.length) {
-            errorCode = 500;
-            throw new Error ("Internal server error. Please, try again later.");
-        };
+        const order = req.query.order as string;
+        const search = req.query.search as string;
+        
+        let products;
+        
+        if (order && order !== "asc" && order !== "desc") throw new Error ("Order must be ascending or descending ('asc'/'desc').");
 
-        res.status(200).send(allProducts);
+        if (!search) {
+
+            if (!order) {
+
+                products = await connection("labecommerce_products")
+                    .select("*");
+                    
+                if (!products.length) {
+                    errorCode = 404;
+                    throw new Error ("Internal server error. Please, try again later.");
+                };
+                res.status(200).send(products);
+
+            } else if (order) {
+
+                products = await connection("labecommerce_products")
+                    .select("*")
+                    .orderBy("name", `${order}`);
+
+                if (!products.length) {
+                    errorCode = 500;
+                    throw new Error ("Internal server error. Please, try again later.");
+                };
+                res.status(200).send(products);
+
+            }
+
+        } else if (search) {
+
+            if (!order) {
+
+                products = await connection("labecommerce_products")
+                    .select("*")
+                    .where({name: search})
+                    .orWhere('name', 'LIKE', `%${search}%`);
+
+                if (!products.length) {
+                    errorCode = 404;
+                    throw new Error ("No products found under this search.");
+                };
+                res.status(200).send(products);
+
+            } else if (order) {
+
+                products = await connection("labecommerce_products")
+                    .select("*")
+                    .where({name: search})
+                    .orWhere('name', 'LIKE', `%${search}%`)
+                    .orderBy("name", `${order}`);
+
+                if (!products.length) {
+                    errorCode = 500;
+                    throw new Error ("No products found under this search.");
+                };
+                res.status(200).send(products);
+
+            }
+        }
 	} catch (error: any) {
 		res.status(errorCode).send(error.message);
 	}
--- modulo5/labEcommerce/src/endpoints/getAllUsers.ts
+++ modulo5/labEcommerce/src/endpoints/getAllUsers.ts
@@ -5,14 +5,32 @@ export const getAllUsers = async (req: Request, res: Response): Promise<void>  =
 	let errorCode = 400;
 
 	try {
-        const allUsers = await connection("labecommerce_users").select("*");
+        let allUsers = await connection("labecommerce_users").select("*");
         if (!allUsers.length) {
             errorCode = 500;
             throw new Error ("Internal server error. Please, try again later.");
         };
 
-        res.status(200).send(allUsers);
+        const userWithPurchases = allUsers.map((user) => {
+            const purchases = getPurchases(user.id);
+            const newUser = {...user, purchases: purchases};
+            return newUser;
+        })
+        if (!userWithPurchases.length) {
+            errorCode = 500;
+            throw new Error ("Internal server error. Please, try again later.");
+        };
+
+        res.status(200).send(userWithPurchases);
 	} catch (error: any) {
-			res.status(errorCode).send(error.message);
+		res.status(errorCode).send(error.message);
 	}
+};
+
+const getPurchases = async (id: string) => {
+    const result = await connection("labecommerce_purchases")
+        .select("*")
+        .where({user_id: id});
+    
+    return [result]
 };
