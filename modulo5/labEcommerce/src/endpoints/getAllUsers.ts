3429e8b4e47d1b7fb1661f9003b378f7f7f5a332
Giovanna Galhardo Julio <gigalhardo98@gmail.com>
2022-09-21 00:07:21 -0300

    final tests and changes

--- modulo5/labEcommerce/src/endpoints/getAllUsers.ts
+++ modulo5/labEcommerce/src/endpoints/getAllUsers.ts
@@ -5,17 +5,24 @@ export const getAllUsers = async (req: Request, res: Response): Promise<void>  =
 	let errorCode = 400;
 
 	try {
-        let allUsers = await connection("labecommerce_users").select("*");
+        const allUsers = await connection("labecommerce_users").select("*");
         if (!allUsers.length) {
             errorCode = 500;
             throw new Error ("Internal server error. Please, try again later.");
         };
 
+        const selectPurchases = await connection("labecommerce_purchases")
+        .select("*")
+
         const userWithPurchases = allUsers.map((user) => {
-            const purchases = getPurchases(user.id);
-            const newUser = {...user, purchases: purchases};
+            const userPurchases = selectPurchases.filter((item) => {
+                return item.user_id === user.id
+            });
+            
+            const newUser = {...user, purchases: userPurchases};
+            
             return newUser;
-        })
+        });
         if (!userWithPurchases.length) {
             errorCode = 500;
             throw new Error ("Internal server error. Please, try again later.");
@@ -25,12 +32,4 @@ export const getAllUsers = async (req: Request, res: Response): Promise<void>  =
 	} catch (error: any) {
 		res.status(errorCode).send(error.message);
 	}
-};
-
-const getPurchases = async (id: string) => {
-    const result = await connection("labecommerce_purchases")
-        .select("*")
-        .where({user_id: id});
-    
-    return [result]
 };
