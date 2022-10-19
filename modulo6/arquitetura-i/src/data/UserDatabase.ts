4d7fc8872a85b4692c12067b3098b12e1d1088eb
Giovanna Galhardo Julio <gigalhardo98@gmail.com>
2022-10-19 19:49:34 -0300

    try/catch database

--- modulo6/arquitetura-i/src/data/UserDatabase.ts
+++ modulo6/arquitetura-i/src/data/UserDatabase.ts
@@ -6,33 +6,52 @@ export class UserDatabase extends BaseDatabase {
     public TABLE_NAME = "User_Arq"
 
     public async getAllUsers() {
-        const result = await UserDatabase.connection(this.TABLE_NAME)
-            .select();
-        return result
+        try {
+            const result = await UserDatabase.connection(this.TABLE_NAME).select();
+
+            return result
+        } catch (error) {
+            throw new Error(error.sqlMessage);
+        };
     }
 
     public async getById(id: string) {
-        const result = await UserDatabase.connection(this.TABLE_NAME)
-            .select()
-            .where({ id });
-        return result
+        try {
+            const result = await UserDatabase.connection(this.TABLE_NAME)
+                            .select()
+                            .where({ id });
+            return result
+        } catch (error) {
+            throw new Error(error.sqlMessage);
+        };
     }
 
     public async getUserByEmail(email: string) {
-        const result = await UserDatabase.connection(this.TABLE_NAME)
-            .select()
-            .where({ email });
-        return result
+        try {
+            const result = await UserDatabase.connection(this.TABLE_NAME)
+                            .select()
+                            .where({ email });
+            return result
+        } catch (error) {
+            throw new Error(error.sqlMessage);
+        };
     }
 
     public async insertUser(user: User) {
-        await UserDatabase.connection(this.TABLE_NAME)
-            .insert(user);
+        try {
+            await UserDatabase.connection(this.TABLE_NAME).insert(user);
+        } catch (error) {
+            throw new Error(error.sqlMessage);
+        };
     }
 
     public async deleteUser(id: string) {
-        await UserDatabase.connection(this.TABLE_NAME)
-            .delete()
-            .where({ id })
+        try {
+            await UserDatabase.connection(this.TABLE_NAME)
+                .delete()
+                .where({ id });
+        } catch (error) {
+            throw new Error(error.sqlMessage);
+        };
     }
 }
